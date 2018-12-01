029ad05d261f47ac1b4f46a80d312e792970ca87
Peter Bocz <bocz.petya@gmail.com>
2018-12-01 21:47:51 +0000

    Day 1 Part 2

---
+++ day01b.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+
+my $filename = 'day01.txt';
+open(my $fh, '<:encoding(UTF-8)', $filename)
+    or die "Could not open file '$filename' $!";
+
+my @changes;
+while (<$fh>) {
+    push(@changes, $_);
+    }
+my $cur_freq;
+my %freqs;
+my $duplicate_freq;
+while (1) {
+    foreach (@changes) {
+        $cur_freq += $_;;
+        if (!$freqs{$cur_freq}) {
+            $freqs{$cur_freq} = 1;
+        } else {
+            print("Duplicate item: $cur_freq\n");
+            $duplicate_freq = $cur_freq;
+            last;
+        }
+    }
+    if ($duplicate_freq) {
+        last;
+    }
+}
